273cc59ed24a5385e2cc10b351ae42e2b9c8345c
Simon Hammerlid <simon.hammerlid@gmail.com>
2024-02-06 17:07:18 +0100

    Added some important scripts and classes

---
+++ My project/Assets/Scripts/Entities/Character.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : Entity
+
+{
+    public bool isFriendly = true;
+    public bool isDead = false;
+    public bool hasActiveAbilityLeft;
+
+    //public BasicAbility basicAbility = //TODO: Lägg basicAbility här;
+    public int moveDistance = 1;
+    //public Life life = //TODO: Lägg basicAbility här;
+
+    public void assignStartingAbilities
+    {
+
+    }
+
+
+
+}
---
+++ My project/Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour
+
+{
+    public bool isFriendly;
+    public bool isDead;
+
+    public BasicAbility basicAbility;
+    public int moveDistance;
+    public Life life;
+
+}
